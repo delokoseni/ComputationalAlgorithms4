6863d785f8c6bfc95ee05f353581723ea78e1621
delokoseni <107359196+delokoseni@users.noreply.github.com>
2024-02-27 19:57:42 +0700

    Нечто

--- ComputationalAlgorithms4.cpp
+++ ComputationalAlgorithms4.cpp
@@ -12,7 +12,11 @@ std::vector<std::vector<double>> GetTableByFunction(std::vector<double> Argument
 std::vector<exprtk::expression<double>> MakeExpressionTable(std::vector<exprtk::symbol_table<double>> SymbolTable,
     std::string ExpressionString, int Size);
 void VectorOutput(std::vector<double> Vector, std::ostream& Stream);
-//std::vector<double> GetDeviations(std::vector<std::vector<double>> Table);
+std::vector<double> GetDeviations(std::vector<std::vector<double>> Table);
+std::vector<std::vector<double>> GetSplineCoefficients(std::vector<std::vector<double>> Table, double PointX);
+std::vector<double> GetIntervals(std::vector<double> X);
+std::vector<double> SweepMethod(std::vector<std::vector<double>> Matrix, std::vector<double> FreeMembersColumn);
+double GetFunctionBySpline(std::vector<std::vector<double>> Table, std::vector<std::vector<double>> SplineCoefficients, double PointX);
 
 int main()
 {
@@ -42,6 +46,38 @@ int main()
     std::vector<double> ArgumentValuesArray(AmountOfValues); //Массив значений аргументов (для режима 2)
     std::vector<double> Deviations(AmountOfValues); //Отклонения
     double MaxDeviation = 0.0; //Максимальное отклонение
+
+    std::vector<std::vector<double>> SplineCoefficients(AmountOfValues, std::vector<double>(AmountOfValues));
+
+
+    /*std::ifstream MatrixFile;
+    MatrixFile.open("Matrix.txt");
+    std::vector<std::vector<double>> Matrix(5, std::vector<double>(5));
+    for (int i = 0; i < 5; i++)
+        for (int j = 0; j < 5; j++)
+            MatrixFile >> Matrix[i][j];
+    std::ifstream ColumnFile;
+    ColumnFile.open("Column.txt");
+    std::vector<double> Column(5);
+    for (int j = 0; j < 5; j++)
+        ColumnFile >> Column[j];
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++)
+            std::cout << Matrix[i][j] << "\t";
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+    for (int j = 0; j < 5; j++)
+        std::cout << Column[j] << "\t";
+    std::cout << std::endl;
+    std::vector<double> X = SweepMethod(Matrix, Column);
+    std::cout << std::endl;
+    for (int j = 0; j < 5; j++)
+        std::cout << X[j] << "\t";
+    std::cout << std::endl;*/
+
+    std::vector<std::vector<double>> Coefficients(AmountOfValues, std::vector<double>(AmountOfValues));
+
     switch (SelectedMode)
     {
     case '1':
@@ -52,6 +88,8 @@ int main()
         double PointX;
         std::cin >> PointX;
         
+        SplineCoefficients = GetSplineCoefficients(Table, PointX);
+        std::cout << GetFunctionBySpline(Table, SplineCoefficients, PointX);
         break;
     case '2':
         std::cout << "Пример функции: sqrt(abs(x))" << std::endl;
@@ -75,6 +113,7 @@ int main()
     }
     FunctionValueTableFile.close();
     ArgumentValuesArrayFile.close();
+
     return 0;
 }
 //Получить количество аргументов из файла
@@ -134,7 +173,6 @@ void TableOutput(std::vector<std::vector<double>> Table, std::ostream& Stream)
     }
 }
 
-
 //Получает таблицу по введенной в аналитическом виде функции
 std::vector<std::vector<double>> GetTableByFunction(std::vector<double> Arguments, std::string ExpressionString)
 {
@@ -172,16 +210,109 @@ std::vector<exprtk::expression<double>> MakeExpressionTable(std::vector<exprtk::
     return Expression; //Возвращается вектор скомпилированных выражений
 }
 //Подсчитывает отклонения
-/*std::vector<double> GetDeviations(std::vector<std::vector<double>> Table)
-{
-    std::vector<double> Deviations(Table[0].size());
-    for (int i = 0; i < Table[0].size(); i++)
-        Deviations[i] = fabs(Table[1][i] - LagrangeInterpolation(Table, Table[0][i]));
-    return Deviations;
-}*/
+//std::vector<double> GetDeviations(std::vector<std::vector<double>> Table)
+//{
+//    std::vector<double> Deviations(Table[0].size());
+//    for (int i = 0; i < Table[0].size(); i++)
+//        Deviations[i] = fabs(Table[1][i] - GetSplineCoefficients(Table, Table[0][i]));
+//    return Deviations;
+//}
 //Вывод вектора
 void VectorOutput(std::vector<double> Vector, std::ostream& Stream)
 {
     for (int i = 0; i < Vector.size(); i++)
         Stream << Vector[i] << std::endl;
+}
+
+//Метод прогонки
+std::vector<double> SweepMethod(std::vector<std::vector<double>> Matrix, std::vector<double> FreeMembersColumn)
+{
+    std::vector<double> Alphas(Matrix[0].size());
+    std::vector<double> Bettas(Matrix[0].size());
+    //Проходим до Matrix[0].size() - 1 т.к. последнее С = 0 -> A = 0
+    for(int  i = 0; i < Matrix[0].size() - 1; i++)
+    {
+        if (i == 0) {
+            Alphas[i] = -Matrix[i][i + 1] / Matrix[i][i]; // C[0] / B[0]
+            Bettas[i] = FreeMembersColumn[i] / Matrix[i][i]; // -D[0] / B[0]
+        }
+        else {
+            Alphas[i] = -Matrix[i][i + 1] / (Matrix[i][i] + Matrix[i][i - 1] * Alphas[i - 1]);
+            Bettas[i] = (FreeMembersColumn[i] - Matrix[i][i - 1] * Bettas[i - 1]) / (Matrix[i][i] + Matrix[i][i - 1] * Alphas[i - 1]);
+        }
+    }
+    int N = Matrix[0].size()-1;
+    Bettas[N] = (FreeMembersColumn[N] - Matrix[N][N - 1] * Bettas[N - 1]) / (Matrix[N][N] + Matrix[N][N - 1] * Alphas[N - 1]);
+
+    std::vector<double> X(Matrix[0].size());
+    for (int i = Matrix[0].size() - 1; i >= 0; i--)
+    {
+        if (i == Matrix[0].size() - 1) {
+            X[i] = Bettas[i];
+        }
+        else {
+            X[i] = Alphas[i] * X[i + 1] + Bettas[i];
+        }
+    }
+    return X;
+}
+//Вычисляет интервалы между значениями X
+std::vector<double> GetIntervals(std::vector<double> X)
+{
+    std::vector<double> H(X.size() - 1);
+    for (int i = 1; i < X.size(); ++i) {
+        H[i - 1] = X[i] - X[i - 1];
+    }
+    return H;
+}
+std::vector<std::vector<double>> GetSplineCoefficients(std::vector<std::vector<double>> Table, double PointX)
+{
+    std::vector<double> H = GetIntervals(Table[0]);
+
+    std::vector<double> FreeMembersColumn(Table[1].size());
+    for (int i = 1; i < FreeMembersColumn.size() - 1; ++i) {
+        FreeMembersColumn[i] = 6 * ((Table[1][i + 1] - Table[1][i]) / H[i] - (Table[1][i] - Table[1][i - 1]) / H[i - 1]);
+    }
+
+    std::vector<std::vector<double>> Matrix;  //В отдельный метод
+    for (int i = 0; i < Table[0].size(); ++i) {
+        std::vector<double> row(Table[0].size(), 0.0);
+        if (i == 0 || i == Table[0].size() - 1) {
+            row[i] = 1.0;
+        }
+        else {
+            row[i - 1] = H[i - 1];
+            row[i] = 2 * (H[i] + H[i - 1]);
+            row[i + 1] = H[i];
+        }
+        Matrix.push_back(row);
+    }
+
+    std::vector<double> C = SweepMethod(Matrix, FreeMembersColumn);
+
+    //H[i-1] везде потому что интервалов на 1 меньше
+    std::vector<double> D(Table[1].size());
+    for (int i = 1; i < D.size(); i++)
+        D[i] = (C[i] - C[i - 1]) / H[i-1];
+
+    std::vector<double> B(Table[1].size());
+    for (int i = 1; i < B.size(); i++)
+        B[i] = (H[i-1] * C[i] / 2) - (H[i-1] * H[i-1] * D[i] / 6) + (Table[1][i] - Table[1][i - 1]) / H[i-1];
+
+
+    return {Table[1], B, C, D};
+}
+
+double GetFunctionBySpline(std::vector<std::vector<double>> Table, std::vector<std::vector<double>> SplineCoefficients, double PointX)
+{
+    for (int i = 1; i < Table[0].size(); ++i) {
+        if (Table[0][i - 1] < PointX && PointX < Table[0][i]) {
+            return SplineCoefficients[0][i] + SplineCoefficients[1][i - 1] * (PointX - Table[0][i]) + SplineCoefficients[2][i - 1] / 2
+                * (PointX - Table[0][i]) * (PointX - Table[0][i]) + SplineCoefficients[3][i - 1] / 6 * (PointX - Table[0][i]) * (PointX - Table[0][i]) * (PointX - Table[0][i]);
+        }
+        if (PointX == Table[0][i]) {
+            return SplineCoefficients[0][i];
+        }
+    }
+    return 0.0;
 }
